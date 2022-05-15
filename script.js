0caf6c9ef03836bf69b517b6185c8b30ec0d4be1
vis277 <vkvishal277@gmail.com>
2022-05-15 09:00:25 +0530

    added comments

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+// Method 1
+/**
+ * 
+ * @param {takes input as 0 and 1 and reverses it} input 
+ * @returns 
+ */
 function reverse(input) {
     return (input + 1) % 2;
   }
   console.log("LN 4- FOR 0 --", reverse(0));
   console.log("LN 5- FOR 1--", reverse(1));
 
+// Method 2
+
   function reverse1(input) {
     return input + 1 - 2 * input;
   }
   console.log("LN 10 input =0:", reverse1(0));
   console.log("LN 11 input =1:", reverse1(1));
+
+// Method 3
 
   function reverse2(input) {
     return Number(!input);
