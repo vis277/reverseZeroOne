16e1d96b22f51f8ca5d4dddb6d45268f2cbc58de
vis277 <vkvishal277@gmail.com>
2022-05-15 08:57:10 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function reverse(input) {
+    return (input + 1) % 2;
+  }
+  console.log("LN 4- FOR 0 --", reverse(0));
+  console.log("LN 5- FOR 1--", reverse(1));
+
+  function reverse1(input) {
+    return input + 1 - 2 * input;
+  }
+  console.log("LN 10 input =0:", reverse1(0));
+  console.log("LN 11 input =1:", reverse1(1));
+
+  function reverse2(input) {
+    return Number(!input);
+  }
+  console.log("LN 16 input =0:", reverse2(0));
+  console.log("LN 17 input =1:", reverse2(1));
